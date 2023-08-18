b416afc9975591724bf96c3dfab7eda5232f9724
YohanaZapata <yohaskj22@gmail.com>
2023-08-18 17:17:02 -0500

    Configuracion de Eureka y se agrego micro servicio a Eureka

--- movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -1,15 +1,15 @@
 package com.dh.movieservice;
 
-import com.dh.movieservice.model.Movie;
-import com.dh.movieservice.repository.MovieRepository;
-import org.springframework.boot.CommandLineRunner;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class MovieServiceApplication {
 
     public static void main(String[] args) {
