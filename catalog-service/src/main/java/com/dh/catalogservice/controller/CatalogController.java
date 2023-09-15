1c18b0c0d6d452f3f0b39f99a653c06eeb93e408
alpadev <alpa.dev@Alejandros-MacBook-Air.local>
2023-09-15 10:05:14 -0500

    circuit breaker

--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -2,23 +2,26 @@ package com.dh.catalogservice.controller;
 
 import com.dh.catalogservice.Repository.IMovieRepository;
 import com.dh.catalogservice.Repository.ISerieRepository;
+import com.dh.catalogservice.Services.MovieService;
 import com.dh.catalogservice.client.IMovieClient;
 import com.dh.catalogservice.client.ISerieClient;
 
 
 import com.dh.catalogservice.model.Movie;
 import com.dh.catalogservice.model.Serie;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 public class CatalogController {
 
     @Autowired
-    private IMovieClient movieClient;
+    private MovieService movieService;
     @Autowired
     private ISerieClient serieClient;
 
@@ -33,15 +36,17 @@ public class CatalogController {
         return ResponseEntity.ok(movieRepository.findByGenre(genre));
     }
 
+
     @PostMapping("/catalog/save")
     ResponseEntity<Movie> saveMovie(@RequestBody Movie movie){
-        return movieClient.saveMovie(movie);
+        return ResponseEntity.ok(movieService.saveMovieService(movie)).getBody();
     }
 
     @GetMapping("/catalog/find/{genre}")
     public ResponseEntity<List<Serie>> getCatalogByGenre2(@PathVariable String genre) {
         return ResponseEntity.ok(serieRepository.findByGenre(genre));
     }
+
 
     @PostMapping("/catalog/saveSerie")
     ResponseEntity<Serie> saveSerie(@RequestBody Serie serie){
